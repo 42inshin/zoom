89261aa656eef286b2b160e94dfa71176a1041d3
inshin <inshin@student.42seoul.kr>
2022-01-02 04:46:16 +0900

    Camera Switch

--- src/server.js
+++ src/server.js
@@ -83,30 +83,3 @@ wsServer.on("connection", socket => {
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
-
-/*
-const wss = new WebSocket.Server({ server });
-const sockets = [];
-
-function onSocketClose() {
-	console.log("Disconnected from the Browser ⛔️")
-}
-
-wss.on("connection", (socket) => {
-	sockets.push(socket);
-	socket["nickname"] = "Anonymous";
-	console.log("Connected to Browser ✅");
-	socket.on("close", onSocketClose);
-	socket.on("message", (msg) => {
-		const message = JSON.parse(msg);
-		switch (message.type) {
-			case "new_message":
-				sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
-				break;
-			case "nickname":
-				socket["nickname"] = message.payload;
-				break;
-		}
-	});
-	// socket.send("hello!!!");
-}); */
