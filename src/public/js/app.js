ea1eabd68d490f268d725b6054f9400550d599d9
inshin <inshin@student.42seoul.kr>
2022-01-02 05:42:42 +0900

    join Rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,18 +5,27 @@ const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 
+const call = document.getElementById("call");
+
+call.hidden = true;
+
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
 
-async function getCamera() {
+async function getCameras() {
 	try {
 		const devices = await navigator.mediaDevices.enumerateDevices();
 		const cameras = devices.filter((device) => device.kind === "videoinput");
+		const currentCamera = myStream.getVideoTracks()[0];
 		cameras.forEach(camera => {
 			const option = document.createElement("option");
 			option.value = camera.deviceId;
 			option.innerText = camera.label;
+			if (currentCamera.label == camera.label) {
+				option.selected = true;
+			}
 			camerasSelect.appendChild(option);
 		});
 	} catch (e) {
@@ -24,20 +33,27 @@ async function getCamera() {
 	}
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+	const initialConstraints = {
+		audio: true,
+		video: { facingMode: "user" },
+	};
+	const cameraConstraints = {
+		audio: true,
+		video: { deviceId: { exact: deviceId } },
+	}
 	try {
-		myStream = await navigator.mediaDevices.getUserMedia({
-			audio: true,
-			video: true,
-		});
+		myStream = await navigator.mediaDevices.getUserMedia(
+			deviceId ? cameraConstraints : initialConstraints
+		);
 		myFace.srcObject = myStream;
-		await getCamera();
+		if (!deviceId) {
+			await getCameras();
+		}
 	} catch (e) {
 		console.log(e);
 	}
 }
-
-getMedia();
 
 function handleMuteClick() {
 	myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
@@ -60,5 +76,37 @@ function handleCameraClick() {
 	}
 }
 
+async function handleCameraChange() {
+	await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form (join a room)
+const welcome = document.getElementById("welcome");
+
+welcomeForm = welcome.querySelector("form");
+
+function startMedia() {
+	welcome.hidden = true;
+	call.hidden = false;
+	getMedia();
+}
+
+function handleWelcomeSubmit(event) {
+	event.preventDefault();
+	const input = welcomeForm.querySelector("input");
+	socket.emit("join_room", input.value, startMedia);
+	roomName = input.value;
+	input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket code
+
+socket.on("welcome", () => {
+	console.log("someone join");
+})
--- src/server.js
+++ src/server.js
@@ -1,18 +1,6 @@
 import http from "http";
-// import WebSocket from "ws";
-import { Server } from 'socket.io';
-import { instrument } from '@socket.io/admin-ui';
+import SocketIO from 'socket.io';
 import express from "express";
-// ES6 버전에선 __dirname 이라는 변수가 기본으로 설정되어 있지 않음
-// babel이 어떠한 이유로 ES5변환을 못하는 경우
-// 예를 들어 package.json 에 type: module 을 추가 할 경우, ES5로 변환을 못하므로
-// 아래의 내용을 추가해야 한다.
-/*
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-*/
 
 const app = express();
 
@@ -23,62 +11,14 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-	cors: {
-		origin: ["https://admin.socket.io"],
-		credentials: true,
-	},
-});
+const wsServer = SocketIO(httpServer);
 
-instrument(wsServer, {
-	auth: false,
-});
-
-function publicRooms(){
-	const {
-		sockets: {
-			adapter: {sids, rooms},
-		},
-	} = wsServer;
-	const publicRooms = [];
-	rooms.forEach((_, key) => {
-		if (sids.get(key) === undefined) {
-			publicRooms.push(key);
-		}
-	});
-	return publicRooms;
-}
-
-function countRoom(roomName) {
-	return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", socket => {
-	wsServer.sockets.emit("room_change", publicRooms());
-	socket["nickname"] = "Anonymous";
-	socket.onAny((event) => {
-		// console.log(wsServer.sockets.adapter);
-		console.log(`socket Event: ${event}`);
-	});
-	socket.on("enter_room", (roomName, done) => {
+wsServer.on("connection", (socket) => {
+	socket.on("join_room", (roomName, done) => {
 		socket.join(roomName);
 		done();
-		socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-		wsServer.sockets.emit("room_change", publicRooms());
+		socket.to(roomName).emit("welcome");
 	});
-	socket.on("disconnecting", () => {
-		socket.rooms.forEach((room) =>
-			socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-		);
-	});
-	socket.on("disconnect", () => {
-		wsServer.sockets.emit("room_change", publicRooms());
-	})
-	socket.on("new_message", (room, msg, done) => {
-		socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-		done();
-	});
-	socket.on("nickname", (nickname) => socket["nickname"] = nickname);
 })
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
